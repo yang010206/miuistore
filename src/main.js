ed52a11623e50e1a2a1d6a09a7ff744f4d006eb5
yang010206 <2093466418@qq.com>
2020-10-10 16:31:42 +0800

    加入组件

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ import store from "./store";
 import Vant from "vant";
 import "vant/lib/index.css";
 import LyTab from "ly-tab";
-
+import "./assets/js/rem.js";
+import "./assets/css/reset.css";
 import axios from "axios";
 Vue.prototype.$http = axios;
 Vue.use(Vant);
